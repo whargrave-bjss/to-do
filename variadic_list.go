c3fc20b914e6e96ceda07d0e9d8d894754a5bab6
whargrave-bjss <will.hargrave@bjss.com>
2024-08-15 16:09:48 +0100

    added Json list function

--- variadic_list.go
+++ variadic_list.go
@@ -1,11 +1,24 @@
 package main
 
-// 10. Create a program using a variadic function to print a list of 10 things To Do. 
+// 10. Create a program using a variadic function to output a list of 10 things To Do to a JSON format file. [Variadic Functions][Structures][JSON]
 
-import "fmt"
+import ("fmt"
+		"encoding/json"
+	)
 func VariadicList(list ...string) []string {
 	for _, n := range list {
 		fmt.Printf("%v\n", n)
 	}
 	return list
 }
+
+func VariadicListJSON(list ...string) string {
+	tasks := make(map[string][]string)
+	tasks["tasks"] = list
+	jsonData, err := json.MarshalIndent(tasks, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("Error: %v", err)
+	}
+	return string(jsonData)
+}
+
--- variadic_list_test.go
+++ variadic_list_test.go
@@ -14,3 +14,13 @@ func VariadicListTest(t *testing.T) {
         t.Errorf("got %v want %v", got, want)
     }
 }
+
+func VariadicListJSONTest(t *testing.T) { 
+
+	got := VariadicListJSON("task1", "task2", "task3", "task4", "task5", "task6", "task7", "task8", "task9", "task10")
+	want := "{\n    \"tasks\": [\n        \"task1\",\n        \"task2\",\n        \"task3\",\n        \"task4\",\n        \"task5\",\n        \"task6\",\n        \"task7\",\n        \"task8\",\n        \"task9\",\n        \"task10\"\n    ]\n}"
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
