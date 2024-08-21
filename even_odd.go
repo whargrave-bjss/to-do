6aa54f2e5f884c2a1f4949761cd03554e38c49e0
whargrave-bjss <will.hargrave@bjss.com>
2024-08-21 12:11:09 +0100

    added synchrosity (not sure if that's a word) to the function to prevent a race condition

--- even_odd.go
+++ even_odd.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"time"
 	"fmt"
+	"sync"
 )
 // Write a program to simulate a race condition occurring when one goroutine updates a data variable with odd numbers, 
 // while another updates the same data variable with even numbers. After each update , 
@@ -12,27 +13,41 @@ import (
 // data variable
 
 var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
-
+ 
 func randomNumber() int {
 	return rng.Intn(10) + 1
 }
-var data int = 0
 
+
+var mutex = &sync.Mutex{}
 func EvenOddSum() {
-	for i := 0; i < 10; i++ {
+	data := make(map[string]int)
+	dataChannel := make(chan int, 50)
+	var wg = sync.WaitGroup{}
+
+	for i := 0; i < 50; i++ {
 		number := randomNumber()
-		fmt.Printf("Generated number: %d\n", number)
-		time.Sleep(1 * time.Second)
+		wg.Add(1)
 		if number%2 == 0 {
-			go func() {
-			data += number
-		    fmt.Printf("Even: %d Added to make: %d\n", number, data)
-			}()
-		} else {
-			go func() {
-			data += number
-			fmt.Printf("Odd: %d Added to make: %d\n", number, data)
-			}()
+			go func(num int) {
+			defer wg.Done()
+			mutex.Lock()
+			defer mutex.Unlock()
+			data["even"] += num
+			dataChannel <- num
+		    fmt.Printf("Even: %d Added to map: %v\n", num, data)
+			}(number)
+		} else {	
+				go func(num int) {
+				defer wg.Done()
+				mutex.Lock()
+				defer mutex.Unlock()
+				data["Odd"] += num
+				dataChannel <- num
+				fmt.Printf("Odd: %d Added to map: %v\n", num, data)
+				}(number)
 		}
 	}
+	wg.Wait()
+	close(dataChannel)
 }
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
+import (
+		"time" 
+		"math/rand"
+	)
 
 func main() {
 	// VariadicList("task1", "task2", "task3", "task4", "task5", "task6", "task7", "task8", "task9", "task10")
 	// VariadicListToJSON("task1", "task2", "task3", "task4", "task5", "task6", "task7", "task8", "task9", "task10")
 	// VariadicJSONFile()
+	rand.NewSource(time.Now().UnixNano())
 	EvenOddSum()
 }
